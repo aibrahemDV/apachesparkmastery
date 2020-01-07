f42644dcd595455e6384e75c3c95b9cb6ada22ce
aibrahemDV <59328678+aibrahemDV@users.noreply.github.com>
2020-01-07 23:31:24 +0400

    Add files via upload

---
+++ Scala-Scripts/DataFrameAggregation.scala
@@ -0,0 +1,144 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types._
+
+import org.apache.spark.sql.functions.{ expr, col, column, desc, asc, coalesce, broadcast}
+import org.apache.spark.sql.functions.{count, countDistinct, min, max, sum, sumDistinct, avg, dense_rank, rank}
+import org.apache.spark.sql.expressions.Window
+import scala.util.matching.Regex
+
+object DataFrameAggregation {
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("akka").setLevel(Level.ERROR)
+
+    val spark = SparkSession.builder.master("local[*]").appName("DataFrame Excerises")
+      .config("spark.sql.autoBroadcastJoinThreshold",1)
+      .config("spark.sql.crossJoin.enable",true)
+      .getOrCreate();
+
+    val sales_schema = StructType(Array(
+      StructField("REQ_ID", IntegerType, true),
+      StructField("EMP_ID", IntegerType, true),
+      StructField("CUSTOMER_ID", IntegerType, true),
+      StructField("PRODUCT_ID", IntegerType, true),
+      StructField("PRICE", IntegerType, true)
+    ))
+
+    val customer_schema =StructType(Array(
+      StructField("CUSTOMER_ID", IntegerType, true),
+      StructField("FIRST_NAME", StringType, true),
+      StructField("LAST_NAME", StringType, true),
+      StructField("EMAIL", StringType, true),
+      StructField("PHONE_NUMBER", StringType, true),
+      StructField("ACTIVATION_DATE", StringType, true)
+    ))
+
+
+    val sales_data = spark.read.format("csv").option("Header", "True").schema(sales_schema)
+      .load("D:\\\\DV\\\\Courses\\\\Apache-Spark\\\\Sources\\\\intelliji-ws\\\\SparkCourse-v2\\\\src\\\\main\\\\resources\\\\sales_data.csv")
+      .toDF()
+
+    val employees_data  = spark.read.format("csv").option("Header", "True").option("InferSchema","true")
+      .load("D:\\\\DV\\\\Courses\\\\Apache-Spark\\\\Sources\\\\intelliji-ws\\\\SparkCourse-v2\\\\src\\\\main\\\\resources\\\\HR_Employees.csv")
+      .toDF()
+
+    val customer_data = spark.read.format("csv").option("Header", "True").schema(customer_schema)
+      .load("D:\\\\DV\\\\Courses\\\\Apache-Spark\\\\Sources\\\\intelliji-ws\\\\SparkCourse-v2\\\\src\\\\main\\\\resources\\\\customer_info.csv")
+      .toDF()
+
+    val trip_data = spark.read.format("csv").option("Header", "True").option("InferSchema","true")
+      .load("D:\\\\DV\\\\Courses\\\\Apache-Spark\\\\Sources\\\\intelliji-ws\\\\SparkCourse-v2\\\\src\\\\main\\\\resources\\\\yellow_tripdata_2017-02.csv")
+      .toDF()
+
+    /***** Count & countDistinct *****/
+
+/*    println(sales_data.count());
+
+    sales_data.select(count("CUSTOMER_ID")).show()
+
+    employees_data.select(count("*")).show()
+    employees_data.select("Region").show()
+    employees_data.select(count("Region")).show()
+
+
+    sales_data.select(countDistinct("CUSTOMER_ID")).show()
+
+    sales_data.groupBy("EMP_ID").agg(count("CUSTOMER_ID")).show() */
+
+    /***** Minimum & Maximum ******/
+
+/*
+    sales_data.select(max(sales_data.col("PRICE"))).show()
+
+    sales_data.select(min("PRICE")).show()
+
+    sales_data.select(min(expr("PRICE * 1000"))).show()
+
+    sales_data.select(min(sales_data.col("PRICE") * 1000 )).show()
+
+    sales_data.select(min("PRICE").as("MAX_PRICE"), max("PRICE").as("MIN_PRICE"), count("*").as("TOTAL_ORDERS")).show()
+
+    sales_data.groupBy("CUSTOMER_ID").agg(max("PRICE").alias("MAX_PURCHASE"), min("PRICE").as("MIN_PURCHASE")).show() */
+
+
+    /***** Sum & Average ******/
+
+/*    sales_data.select(sum("PRICE").alias("TOTAL_PURCHASE")).show()
+    sales_data.select(sumDistinct("PRICE").alias("TOTAL_PURCHASE")).show()
+    sales_data.groupBy("PRODUCT_ID").agg(sum("PRICE")).orderBy("PRODUCT_ID").show()
+    sales_data.groupBy("PRODUCT_ID").sum("PRICE").orderBy("PRODUCT_ID").show()
+
+    sales_data.groupBy("CUSTOMER_ID").agg(avg("PRICE").alias("AVERAGE_PURCHASE")).show()
+    sales_data.groupBy("CUSTOMER_ID").avg("PRICE").orderBy("CUSTOMER_ID").show()*/
+
+
+    /**** EXCERISE ****************/
+/*    val joinCondition = sales_data.col("CUSTOMER_ID") === customer_data.col("CUSTOMER_ID")
+    sales_data.as("sales").join(customer_data.as("customer"),joinCondition,"inner").
+      select(expr("sales.CUSTOMER_ID"),expr("sales.PRICE"),expr("customer.FIRST_NAME||' '||customer.LAST_NAME as FULL_NAME"),expr("sales.PRODUCT_ID"),expr("sales.EMP_ID"))
+      .groupBy("sales.CUSTOMER_ID","FULL_NAME")
+      .agg(avg("PRICE").alias("AVG_PURCHASE"),max("PRICE").alias("MAX_PURCHASE"),min("PRICE").alias("MIN_PURCHASE"),
+        count("PRODUCT_ID").alias("PRODUCT_PURCHASED"), countDistinct("EMP_ID").alias("EMPLOYEES_ASSIGNED")).show()*/
+
+
+    /***** Analytics ******/
+
+    val tripDF = trip_data.select("VendorID","passenger_count","trip_distance","payment_type","fare_amount","tip_amount","total_amount")
+
+    val windowDetails = Window.partitionBy("payment_type").orderBy(col("total_amount").asc)
+      .rowsBetween(Window.unboundedPreceding,Window.currentRow)
+    val sumTotalFare = sum(col("total_amount")).over(windowDetails)
+    val maxTotalFare = max(col("total_amount")).over(windowDetails)
+    val fareDenseRank = dense_rank().over(windowDetails)
+    val fareRank = rank().over(windowDetails)
+
+    tripDF.select(col("payment_type"),col("total_amount"),
+      sumTotalFare.alias("TD_Total_Fare"),
+      maxTotalFare.alias("TD_MAX_Fare"),
+      fareRank.alias("TD_RANK"),
+      fareDenseRank.alias("Dense_Rank")).show()
+
+
+        val windowProperties = Window.partitionBy("CUSTOMER_ID").orderBy(col("PRICE").desc)
+                           .rowsBetween(Window.unboundedPreceding,Window.currentRow)
+        val sumPurchaseQuantity = sum(col("PRICE")).over(windowProperties)
+        val maxPurchaseQuantity = max(col("PRICE")).over(windowProperties)
+
+        val purchaseDenseRank = dense_rank().over(windowProperties)
+        val purchaseRank = rank().over(windowProperties)
+
+        sales_data.select(col("REQ_ID"),col("CUSTOMER_ID"),col("PRICE"),
+          sumPurchaseQuantity.alias("TD_TOTAL_PURCHASE"),
+          maxPurchaseQuantity.alias("TD_MAX_PURCHASE"),
+          purchaseDenseRank.alias("DENSE_RANK"),
+          purchaseRank.alias("RANK")).orderBy("CUSTOMER_ID").show()
+
+
+
+
+
+  }
+
+}
---
+++ Scala-Scripts/PartitioningAndSavingData.scala
@@ -0,0 +1,190 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions.{col, expr}
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
+
+object PartitioningAndSavingData {
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+
+    val spark = SparkSession.builder.master("local[*]").appName("DataFrame Excerises")
+      .config("spark.sql.shuffle.partitions","150")
+      .getOrCreate();
+
+    //.config("spark.sql.warehouse.dir", "target/spark-warehouse")
+
+
+    val employees_schema = StructType(Array(
+      StructField("NAME",StringType,true),
+      StructField("JOB_TITLE",StringType,true),
+      StructField("DEPARTEMENT",StringType,true),
+      StructField("FULL_PART_TIME",StringType,true),
+      StructField("MONTHLY_HOURLY",StringType,true),
+      StructField("TYPICAL_HOURS",IntegerType,true),
+      StructField("ANNUAL_SALARY",DoubleType,true),
+      StructField("HOURLY_RATE",DoubleType,true)
+    ))
+
+
+    val employees_data = spark.read.format("csv").option("Header","True").schema(employees_schema)
+      .load("D:\\\\DV\\\\Courses\\\\Apache-Spark\\\\Sources\\\\intelliji-ws\\\\SparkCourse-v2\\\\src\\\\main\\\\resources\\\\Employee_Data.csv")
+      .toDF();
+
+
+
+/******************** Partition DataFrame ********************************** */
+    // println(employees_data.rdd.getNumPartitions)
+    //println("Number of Partitions: "+ employees_data_partitioned.rdd.partitions.size)
+    /*      val employees_data_partitioned = spark.read.format("csv").option("Header","True").schema(employees_schema)
+            .load("D:\\\\DV\\\\Courses\\\\Apache-Spark\\\\Sources\\\\intelliji-ws\\\\SparkCourse-v2\\\\src\\\\main\\\\resources\\\\Employee_Data.csv")
+            .repartition(10);*/
+
+    /*      println(employees_data.select("DEPARTEMENT").distinct().count())
+            println()
+            println("Number of Partitions: "+ employees_data_partitioned.rdd.getNumPartitions)
+
+           val employees_data_partitioned = spark.read.format("csv").option("Header","True").schema(employees_schema)
+           .load("D:\\\\DV\\\\Courses\\\\Apache-Spark\\\\Sources\\\\intelliji-ws\\\\SparkCourse-v2\\\\src\\\\main\\\\resources\\\\Employee_Data.csv")
+           .repartition(col("DEPARTEMENT"));*/
+
+    /*      val employees_data_partitioned = spark.read.format("csv").option("Header","True").schema(employees_schema)
+            .load("D:\\\\DV\\\\Courses\\\\Apache-Spark\\\\Sources\\\\intelliji-ws\\\\SparkCourse-v2\\\\src\\\\main\\\\resources\\\\Employee_Data.csv")
+            .repartition(15,col("DEPARTEMENT"));
+
+           println()
+           println("Number of Partitions: "+ employees_data_partitioned.rdd.getNumPartitions)*/
+
+
+/*    val employees_data_partitioned = employees_data.repartition(15,col("DEPARTEMENT"))
+
+    println()
+    println("Number of Partitions (Original DF): "+ employees_data.rdd.getNumPartitions)
+    println()
+    println("Number of Partitions (Partitioned DF): "+ employees_data_partitioned.rdd.getNumPartitions)*/
+
+
+/******************** Save DataFrame data ********************************** */
+
+//------------------------- CSV --------------------------------------------//
+/*
+      employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write.format("csv").save("D:\\Target-Data\\CSV\\employee_data.csv")
+      employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write.csv("D:\\Target-Data\\CSV\\employee_data.csv")
+*/
+
+/*
+    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+                   .option("sep","|").option("nullValue","UNIDENTIFIED")
+                   .format("csv").save("D:\\Target-Data\\CSV\\employee_data.csv")
+
+    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+                  .option("sep","|").option("nullValue","UNIDENTIFIED")
+                  .csv("D:\\Target-Data\\CSV\\employee_data.csv")
+*/
+
+//------------------------- JSON --------------------------------------------//
+
+/*
+
+    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write.format("json").save("D:\\Target-Data\\JSON\\employee_data.json")
+    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write.json("D:\\Target-Data\\JSON\\employee_data.json")
+*/
+
+
+//------------------------- Text --------------------------------------------//
+
+
+/*
+   employees_data.select("NAME","DEPARTEMENT","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+     .partitionBy("DEPARTEMENT","FULL_PART_TIME")
+     .format("text").save("D:\\Target-Data\\TXT\\employee_data.txt")
+
+   employees_data.select("NAME","DEPARTEMENT","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+     .partitionBy("DEPARTEMENT","FULL_PART_TIME")
+     .mode(SaveMode.Overwrite).text("D:\\Target-Data\\TXT\\employee_data.txt")
+*/
+
+   //employees_data.repartition(1).rdd.saveAsTextFile("D:\\Target-Data\\TXT\\employee_data.txt")
+
+
+//------------------------- Parquet --------------------------------------------//
+
+
+/*
+        employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+                      .format("parquet").save("D:\\Target-Data\\PARQUET\\employee_data.parquet")
+
+        employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+                      .parquet("D:\\Target-Data\\PARQUET\\employee_data.parquet")
+*/
+
+
+//------------------------- JDBC --------------------------------------------//
+
+
+val prop = new java.util.Properties()
+    prop.put("user","sparkusr")
+    prop.put("password","sparkusr")
+    prop.put("useSSL","false")
+    prop.put("allowPublicKeyRetrieval","true")
+    prop.put("driver","com.mysql.cj.jdbc.Driver")
+
+
+    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+      .jdbc("jdbc:mysql://localhost:3306/employeesdb","employeesdb.employees_data", prop)
+
+
+/*
+    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+      .mode(SaveMode.Overwrite).format("jdbc")
+      .option("driver","com.mysql.jdbc.Driver")
+      .option("url","jdbc:mysql://localhost:3306/employeesdb")
+      .option("user","root")
+      .option("dbtable","employeesdb.employees_data")
+      .option("useSSL","false")
+      .option("allowPublicKeyRetrieval","true")
+*/
+
+
+
+    val data = spark.read
+      .jdbc("jdbc:mysql://localhost:3306/employeesdb","employeesdb.dept_manager", prop)
+
+
+    data.show()
+
+//--------------------- Partition and Sort output --------------------------------------------//
+
+/*
+    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME","DEPARTEMENT").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write
+      .partitionBy("DEPARTEMENT","FULL_PART_TIME")
+      .option("sep","|").option("nullValue","UNIDENTIFIED")
+      .json("D:\\Target-Data\\CSV\\employee_data.csv")
+*/
+
+
+
+//--------------------- Handle Conflict  --------------------------------------------//
+/*    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write.mode(SaveMode.Append)
+      .option("sep","|").option("nullValue","UNIDENTIFIED")
+      .format("csv").save("D:\\Target-Data\\CSV\\employee_data.csv")
+
+    employees_data.select("NAME","JOB_TITLE","FULL_PART_TIME").where(expr("DEPARTEMENT IN ('TRANSPORTN','FIRE')")).write.mode(SaveMode.ErrorIfExists)
+      .option("sep","|").option("nullValue","UNIDENTIFIED")
+      .csv("D:\\Target-Data\\CSV\\employee_data.csv")*/
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+  }
